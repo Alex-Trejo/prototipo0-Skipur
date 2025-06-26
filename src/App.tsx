931485db3d1029902f1f390a8bb95eb2d2269579
Alexander-Guacan <adguacan@espe.edu.ec>
2025-06-26 10:34:16 -0500

    Protect routes by user role

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,19 @@ import { Register } from './pages/Register'
 import { AdminLayout } from './components/layouts/Admin'
 import { AdminHome } from './pages/Admin'
 import { Specialties } from './pages/Specialties'
+import { ProtectedRoute } from './components/auth/ProtectedRoute'
 
 function App() {
   return (
     <Routes>
       <Route index element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/admin" element={<AdminLayout />}>
-        <Route index element={<AdminHome />} />
-        <Route path="specialties" element={<Specialties />} />
+
+      <Route element={<ProtectedRoute allowedRoles={['ADMIN']} />}>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminHome />} />
+          <Route path="specialties" element={<Specialties />} />
+        </Route>
       </Route>
     </Routes>
   )
--- src/constants/auth.constants.ts
+++ src/constants/auth.constants.ts
@@ -1,1 +1,9 @@
+import type { UserRole } from '../types/user'
+
 export const ACCESS_TOKEN_KEY = 'access_token'
+
+export const HOME_PATH_BY_ROLE: Record<UserRole, string> = {
+  ADMIN: '/admin',
+  CLIENTE: '/client',
+  ESPECIALISTA: '/specialist',
+}

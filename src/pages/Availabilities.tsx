87ec2c486d5379682135bc00c7f23b0060d18f63
Alexander-Guacan <adguacan@espe.edu.ec>
2025-07-08 07:27:25 -0500

    Load availabilities of specialist

--- src/pages/Availabilities.tsx
+++ src/pages/Availabilities.tsx
@@ -1,6 +1,15 @@
 import { SearchForm } from '../components/forms/SearchForm'
+import { AvailabilitiesTable } from '../components/tables/AvailabilitiesTable'
+import { useAuth } from '../hooks/useAuth'
+import { useAvailabilities } from '../hooks/useAvailabilities'
 
 export function Availabilities() {
+  const { authUser } = useAuth()
+  const { availabilities, loading } = useAvailabilities({
+    userId: authUser?.id,
+    startTime: new Date('2024-01-01T00:00:00Z').toString(),
+    endTime: Date.now().toString(),
+  })
   return (
     <main className="p-8">
       <h1 className="text-4xl font-semibold">Gestionar Especialistas</h1>
@@ -10,9 +19,10 @@ export function Availabilities() {
           className="bg-blue-500 text-white p-2 rounded-md  h-fit"
           type="button"
         >
-          Agregar Disponibilidad
+          Agregar Horario
         </button>
       </div>
+      <AvailabilitiesTable availabilities={availabilities} loading={loading} />
     </main>
   )
 }
